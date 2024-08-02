37beb6b458d676ea4a3c4f8beab6b53ebf6e2ece
Techer Pierre <techerpierre.pro@gmail.com>
2024-08-02 16:17:56 +0200

    setup for node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-import Engine from "./engine";
+export { default  as Engine} from "./engine";
 
-export default function RenderApp(root: ReactDom.Container) {
+/*export default function RenderApp(root: ReactDom.Container) {
     ReactDom.createRoot(root).render(
         React.createElement(Engine, 
             { elements: { "/": { type: "ELEMENT", key: "p", children: "Salut à tous" }} }
         ))
-}
+}*/
